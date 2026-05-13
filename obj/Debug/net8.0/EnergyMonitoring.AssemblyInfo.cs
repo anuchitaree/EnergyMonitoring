29955c356c8b04b269b69ad3cb26ca6bab4a62b6
diatanuchit <anuchit.aree.a9f@ap.denso.com>
2026-05-13 15:30:09 +0700

    test

--- obj/Debug/net8.0/EnergyMonitoring.AssemblyInfo.cs
+++ obj/Debug/net8.0/EnergyMonitoring.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnergyMonitoring")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cde1261fcc0637a7d861511df4fbc28f848c8e5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3811722d86bc4bcad0baec1b2ff9f2b95e3c3b84")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnergyMonitoring")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnergyMonitoring")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
